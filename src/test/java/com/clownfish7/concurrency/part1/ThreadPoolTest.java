279bd39f98f5cc701121b5d1dc1fd96d1c112297
clownfish7 <279505647@qq.com>
2020-04-26 18:23:13 +0800

    ~

--- src/test/java/com/clownfish7/concurrency/part1/ThreadPoolTest.java
+++ src/test/java/com/clownfish7/concurrency/part1/ThreadPoolTest.java
@@ -1,6 +1,12 @@
 package com.clownfish7.concurrency.part1;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
  * @author You
@@ -8,13 +14,34 @@ import java.util.LinkedList;
  */
 public class ThreadPoolTest {
 
+    @Test
+    public void test() throws InterruptedException {
+        SimpleThreadPool threadPool = new SimpleThreadPool();
+        IntStream.rangeClosed(0, 40).forEach(i -> {
+            threadPool.submit(() -> {
+                System.out.println("the runnable begining " + i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("the runnable finished " + i);
+            });
+        });
+        Thread.currentThread().join(10_000);
+    }
 
 }
 
 class SimpleThreadPool {
 
     private final int size;
-    private final static int DEEFAULT_SIZE = 10;
+    private static final int DEEFAULT_SIZE = 10;
+    private static volatile int seq = 0;
+    private static final String THREAD_PREFIX = "Thread_Pool-";
+    private static final LinkedList<Runnable> TASK_QUEUE = new LinkedList<>();
+    private static final List<WorkerTask> THREAD_QUEUE = new ArrayList<>();
+    private static final ThreadGroup GROUP = new ThreadGroup("Pool_Group");
 
     public SimpleThreadPool() {
         this(DEEFAULT_SIZE);
@@ -26,6 +53,20 @@ class SimpleThreadPool {
     }
 
     private void init() {
+        IntStream.range(0, size).forEach(i -> createWorkTask(i + ""));
+    }
+
+    public void submit(Runnable runnable) {
+        synchronized (TASK_QUEUE) {
+            TASK_QUEUE.addLast(runnable);
+            TASK_QUEUE.notifyAll();
+        }
+    }
+
+    private void createWorkTask(String name) {
+        WorkerTask workerTask = new WorkerTask(GROUP, THREAD_PREFIX + name);
+        workerTask.start();
+        THREAD_QUEUE.add(workerTask);
     }
 
     private enum TaskState {
@@ -35,14 +76,12 @@ class SimpleThreadPool {
         DEAD
     }
 
-    private static class WorkerThread extends Thread {
+    private static class WorkerTask extends Thread {
 
         private volatile TaskState taskState = TaskState.FREE;
 
-        private final LinkedList<Runnable> TASK_QUEUE = new LinkedList<>();
-
-        public WorkerThread(ThreadGroup group, String name) {
-            super(group,name);
+        public WorkerTask(ThreadGroup group, String name) {
+            super(group, name);
         }
 
         public TaskState getTaskState() {
@@ -57,16 +96,22 @@ class SimpleThreadPool {
         public void run() {
             outer:
             while (this.taskState != TaskState.DEAD) {
-                synchronized (TASK_QUEUE){
+                Runnable runnable;
+                synchronized (TASK_QUEUE) {
                     while (TASK_QUEUE.isEmpty()) {
                         try {
+                            this.taskState = TaskState.BLOCKED;
                             TASK_QUEUE.wait();
                         } catch (InterruptedException e) {
                             break outer;
                         }
                     }
-                    Runnable runnable = TASK_QUEUE.removeFirst();
-                    //TODO ThreadPool
+                    runnable = TASK_QUEUE.removeFirst();
+                }
+                if (runnable != null) {
+                    this.taskState = TaskState.RUNNING;
+                    runnable.run();
+                    this.taskState = TaskState.FREE;
                 }
             }
         }
